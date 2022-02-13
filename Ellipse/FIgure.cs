51ece3e2a7f548a5fe46443832cb3dfe3fabbf0b
Alexander Pirogov <San15ka@yandex.ru>
2022-02-13 15:29:17 +0300

    Класс для работы с эллипсом: расчет длины и площади эллипса

---
+++ Ellipse/FIgure.cs
@@ -0,0 +1,36 @@
+﻿namespace Ellipse
+{
+    public class Figure
+    {
+        public Point Center;
+        public int HorizontalRadius { get; set; }
+        public int VerticalRadius { get; set; }
+        public Figure(Point center, int horizontalRadius, int verticalRadius)
+        {
+            HorizontalRadius = checkRadius(horizontalRadius);
+            VerticalRadius = checkRadius(verticalRadius);
+
+            Center = center;
+        }
+
+        public double GetSquare()
+        {
+            return Math.PI * HorizontalRadius * VerticalRadius;
+        }
+
+        public double GetLength()
+        {
+            return Math.PI * (HorizontalRadius + VerticalRadius);
+        }
+
+        protected int checkRadius(int radius)
+        {
+            if (radius <= 0)
+            {
+                throw new ArgumentException("Radius cannot be negative");
+            }
+
+            return radius;
+        }
+    }
+}
